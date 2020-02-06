3a16371b5afe75806dae8e74e78539574f144ff0
David Smith <davsm2@yahoo.se>
2020-02-06 22:02:56 +0100

    First try at adding profiles from called services

--- src/import/dips-profiling.ts
+++ src/import/dips-profiling.ts
@@ -1,5 +1,5 @@
 import {CallTreeProfileBuilder, ProfileGroup, FrameInfo} from '../lib/profile'
-import {ProfileDataSource} from '../import/utils'
+import {ProfileDataSource, MultiFileDataSource} from '../import/utils'
 
 // Custom import for DIPS profiling logs
 
@@ -17,6 +17,11 @@ interface ProfCall {
   // calculated
   parent: ProfCall | null
   children: ProfCall[]
+}
+
+interface ImportFile {
+  calls: ProfCall[]
+  callsTo: Map<ImportFile, number>
 }
 
 //const ColVersion = 0
@@ -82,6 +87,16 @@ function parseCall(rawEvent: string): ProfCall {
   };
 }
 
+function setUpRelationsOnAllCalls(files: ImportFile[]) {
+  let calls : ProfCall[] = []
+  
+  for (let file of files) {
+    calls = calls.concat(file.calls)
+  }
+
+  setUpRelations(calls)
+}
+
 function setUpRelations(calls: ProfCall[]) {
   const callIdMap = new Map<string, ProfCall>()
 
@@ -99,7 +114,7 @@ function setUpRelations(calls: ProfCall[]) {
   }  
 }
 
-function startTimestampsAtZero(calls: ProfCall[]) {
+function timeOfFirstCall(calls: ProfCall[]) : number {
   // Find first timestamp. It should be the first top-level call
   // but we might as well check all
   let min = Number.MAX_SAFE_INTEGER
@@ -108,8 +123,14 @@ function startTimestampsAtZero(calls: ProfCall[]) {
     if (call.at < min)
       min = call.at
 
-  for (let call of calls) {
-    call.at -= min
+  return min
+}
+
+function translateByOffset(files : ImportFile[], offset : number) {
+  for (let file of files) {
+    for (let call of file.calls) {
+      call.at += offset
+    }
   }
 }
 
@@ -127,7 +148,7 @@ function getBuilderForCall(call: ProfCall, builders: MyBuilder[], width : number
   return found
 }
 
-function ensureWithinInterval(value: number, min: number, max: number) : number {
+function adjustToInterval(value: number, min: number, max: number) : number {
   if (value < min) return min
   if (value > max) return max
   return value
@@ -145,8 +166,8 @@ function addFrames(call: ProfCall, builder: MyBuilder, parent_end: number) {
   // We need to adjust the times b/c timestamps and elapsed times are not
   // exact and sometimes there is a millisecond overlap, which would cause
   // the builder to fail
-  let start = ensureWithinInterval(call.at, builder.lastEventAt, parent_end)
-  let end = ensureWithinInterval(start + call.elapsed, start, parent_end)
+  let start = adjustToInterval(call.at, builder.lastEventAt, parent_end)
+  let end = adjustToInterval(start + call.elapsed, start, parent_end)
 
   try {
     builder.enterFrame(frameInfo, start)
@@ -165,32 +186,106 @@ function addFrames(call: ProfCall, builder: MyBuilder, parent_end: number) {
   }
 }
 
-export async function importDIPSProfiling(dataSource: ProfileDataSource): Promise<ProfileGroup | null> {
+async function parseFile(dataSource: ProfileDataSource): Promise<ImportFile> {
   let contents = await dataSource.readAsText()
 
   const calls = contents.split('\r\n')
                         .filter(s => s.startsWith('1;'))
                         .map(parseCall)
 
-  if (calls.length == 0) {
+  return {
+    calls: calls,
+    callsTo: new Map<ImportFile, number>()
+  }
+}
+
+function guessTopLevelFile(files: ImportFile[]): ImportFile {
+  if (files.length == 1) {
+    // quick return for performance
+    return files[0]
+  }
+
+  let callMap = new Map()
+
+  // Map callID -> file
+  for (let file of files) {
+    for (let call of file.calls) {
+      callMap.set(call.callID, file)
+    }
+  }
+
+  // Fill in callsTo
+  for (let file of files) {
+    for (let call of file.calls) {
+      const parent = callMap.get(call.parentCallID)
+      if (parent && parent !== file) {
+        const n = parent.callsTo.get(file) || 0
+        parent.callsTo.set(file, n+1)
+      }
+    }
+  }
+
+  // Find the first file in the call chain, i.e. from the service that calls other services
+  let first = files[0]
+
+  for (let file of files)
+  {
+    if ((file.callsTo.get(first) || 0) > (first.callsTo.get(file) || 0)) {
+      first = file
+    }
+  }
+
+  return first
+}
+
+function filterByRootIDs(files: ImportFile[], rootIDs : Set<string>) {
+  for (var file of files)
+    file.calls = file.calls.filter(c => rootIDs.has(c.rootCallID))
+}
+
+function toSet<T>(array : Array<T>) : Set<T> {
+  const set = new Set<T>()
+  for (var item of array) {
+    set.add(item)
+  }
+  return set
+}
+
+export async function importDIPSProfiling(dataSource: ProfileDataSource): Promise<ProfileGroup | null> {
+  let dataSources = (dataSource instanceof MultiFileDataSource) ?
+                        (dataSource as MultiFileDataSource).dataSources : [dataSource]
+
+  let files = await Promise.all(dataSources.map(d => parseFile(d)))
+  
+  files = files.filter(f => f.calls.length > 0)
+  
+  if (files.length == 0) {
     return null
   }
 
-  setUpRelations(calls)
-  startTimestampsAtZero(calls)
+  // We'll only show calls from the first file in the call order. The 
+  // others are called from this and are just adding frames to the profile.
 
-  let width = 0
-  for (let call of calls)
-    if (call.at + call.elapsed > width)
-      width = call.at + call.elapsed
-  
+  const primaryFile = guessTopLevelFile(files)
+  const extraFiles = files.filter(f => f !== primaryFile)
+
+  const rootCallIDs = toSet(primaryFile.calls.map(c => c.rootCallID))
+
+  filterByRootIDs(extraFiles, rootCallIDs)
+
+  const offset = -timeOfFirstCall(primaryFile.calls)
+
+  translateByOffset(files, offset)
+
+  setUpRelationsOnAllCalls(files)
+
+  const topCalls = primaryFile.calls.filter(c => c.parent == null)
+  const width = topCalls[topCalls.length-1].at + topCalls[topCalls.length-1].elapsed  
   const profiles : MyBuilder[] = []
 
-  for (let call of calls) {
-    if (!call.parent) {
-      const builder = getBuilderForCall(call, profiles, width)
-      addFrames(call, builder, width)
-    }
+  for (let call of topCalls) {
+    const builder = getBuilderForCall(call, profiles, width)
+    addFrames(call, builder, width)
   }
 
   return {
